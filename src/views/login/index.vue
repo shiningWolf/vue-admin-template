c757b84f4897fa1a086594a15557ca32fef77ee6
leizore <zhaolei_just@163.com>
2020-02-18 16:52:21 +0800

    分支问题

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -110,6 +110,7 @@ export default {
         if (valid) {
           this.loading = true
           this.$store.dispatch('user/login', this.loginForm).then(() => {
+            sessionStorage.setItem('user', this.loginForm.username)
             this.$router.push({ path: this.redirect || '/' })
             this.loading = false
           }).catch(() => {
